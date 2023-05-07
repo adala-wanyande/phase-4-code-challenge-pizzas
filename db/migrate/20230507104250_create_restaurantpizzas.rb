71afb4b2a2427964d522569be59b43251573ad99
benard-dev <benardwanyande@gmail.com>
2023-05-07 14:02:44 +0300

    feat: Add validation to the RestaurantPizza model
    Ensures the price of pizza is between 1 and 30. Also, small fix to RestaurantPizza model and migrations.

--- db/migrate/20230507104250_create_restaurantpizzas.rb
+++ db/migrate/20230507104250_create_restaurantpizzas.rb
@@ -1,4 +1,4 @@
-class CreateRestaurantpizzas < ActiveRecord::Migration[7.0]
+class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurant_pizzas do |t|
       t.integer :pizza_id
